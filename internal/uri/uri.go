04b7ee4b45c8545c7bac81cfe86374e2e0e61798
James Pogran <jpogran@outlook.com>
2023-12-18 09:45:03 -0500

    Alert on wsl.localhost Path

--- internal/uri/uri.go
+++ internal/uri/uri.go
@@ -206,5 +206,13 @@ func IsWSLURI(uri string) bool {
 		return false
 	}
 
+	if u.Scheme == "file" && u.Host == "wsl$" {
+		return true
+	}
+
+	if u.Scheme == "file" && u.Host == "wsl.localhost" {
+		return true
+	}
+
 	return u.Scheme == "file" && u.Host == "wsl$"
 }
